a039ff1700ee61ce34349995d72db64b5d197083
Mikhail.udot <Mikhail.udot@venafi.com>
2018-09-19 22:55:02 +0300

    Add Vehicle Update method

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -23,31 +23,64 @@ namespace vega.Controllers {
             if(!ModelState.IsValid) {
                 return BadRequest(ModelState);
             }
-            var model = await _context.Models.FindAsync(vehicle.ModelId);
+
+            if(!await ValidateVehicle(vehicle)) {
+                return BadRequest(ModelState);
+            }
+
+            try {               
+                Vehicle domainVehicle = _mapper.Map<VehicleResource, Vehicle>(vehicle);
+                domainVehicle.LastUpdate = DateTime.Now;
+                _context.Vehicles.Add(domainVehicle);
+                await _context.SaveChangesAsync();
+                VehicleResource result = _mapper.Map<Vehicle, VehicleResource>(domainVehicle);
+                return Ok(result);
+            } catch(Exception e) {
+                return StatusCode(500, e.InnerException?.Message ?? e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id, [FromBody] VehicleResource vehicle) {
+            if(!ModelState.IsValid) {
+                return BadRequest(ModelState);
+            }
+
+            var domainVehicle = await _context.Vehicles.FindAsync(id);
+            if(domainVehicle ==  null) {
+                ModelState.AddModelError("id",$"Model with Id = {id} not found!");
+            }
+           
+            if(!await ValidateVehicle(vehicle)) {
+                return BadRequest(ModelState);
+            }
+
+            try {               
+                _mapper.Map<VehicleResource,Vehicle>(vehicle, domainVehicle);
+                domainVehicle.LastUpdate = DateTime.Now;
+                await _context.SaveChangesAsync();
+                VehicleResource result = _mapper.Map<Vehicle, VehicleResource>(domainVehicle);
+                return Ok(result);
+            } catch(Exception e) {
+                return StatusCode(500, e.InnerException?.Message ?? e.Message);
+            }
+        }
+
+        private async Task<bool> ValidateVehicle(VehicleResource vehicle) {
+            Model model = await _context.Models.FindAsync(vehicle.ModelId);
             if(model ==  null) {
                 ModelState.AddModelError("ModelId",$"Cannot find model with Id = {vehicle.ModelId}");
             }
             if(!vehicle.Features.Any()) {
                 ModelState.AddModelError("Features","Please specify features"); 
             } 
-            foreach(var id in vehicle.Features) {
-                if(await _context.Features.FindAsync(id) == null) {
-                     ModelState.AddModelError("Features",$"Cannot find feature with Id = {id}");
+            foreach(var featureId in vehicle.Features) {
+                if(await _context.Features.FindAsync(featureId) == null) {
+                     ModelState.AddModelError("Features",$"Cannot find feature with Id = {featureId}");
                 }
             }
-            if(ModelState.ErrorCount > 0) {
-                return BadRequest(ModelState);
-            }
-            try {               
-                 Vehicle vehicleModel = _mapper.Map<VehicleResource, Vehicle>(vehicle);
-                vehicleModel.LastUpdate = DateTime.Now;
-                _context.Vehicles.Add(vehicleModel);
-                await _context.SaveChangesAsync();
-                VehicleResource result = _mapper.Map<Vehicle, VehicleResource>(vehicleModel);
-                return Ok(result);
-            } catch(Exception e) {
-                return StatusCode(500, e.InnerException?.Message ?? e.Message);
-            }
+
+            return ModelState.ErrorCount == 0;
         }
     }
 }

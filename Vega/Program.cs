7390153d22fd3d6ad9e5c6813307f8f97675839f
Mikhail Udot <mikhail.udot@venafi.com>
2019-02-26 16:01:07 +0300

    Implement HTTPS protocol requirement

--- Vega/Program.cs
+++ Vega/Program.cs
@@ -1,25 +1,43 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.FileProviders;
 
-namespace vega
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+namespace vega {
+	public class Program {
+		public static void Main(string[] args) {
+			BuildWebHost(args).Run();
+		}
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+		public static IWebHost BuildWebHost(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.UseKestrel(options => {
+					options.Listen(IPAddress.Loopback, 44360, listenOptions => {
+						var serverCertificate = LoadCertificate();
+						listenOptions.UseHttps(serverCertificate); // <- Configures SSL
+					});
+				})
+				.Build();
+
+		private static X509Certificate2 LoadCertificate() {
+			var assembly = typeof(Startup).GetTypeInfo().Assembly;
+			var embeddedFileProvider = new EmbeddedFileProvider(assembly, "vega");
+			var certificateFileInfo = embeddedFileProvider.GetFileInfo(Path.Combine("Certificates","localhost.pfx"));
+			using (var certificateStream = certificateFileInfo.CreateReadStream()) {
+				byte[] certificatePayload;
+				using (var memoryStream = new MemoryStream()) {
+					certificateStream.CopyTo(memoryStream);
+					certificatePayload = memoryStream.ToArray();
+				}
+
+				return new X509Certificate2(certificatePayload, "passw0rd!");
+			}
+		}
+	}
+
+	
 }
